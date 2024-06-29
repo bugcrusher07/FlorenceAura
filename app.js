fdd088da46fab30859d37b71c1c9731ece5d0fb6
bugcrusher07 <165899073+bugcrusher07@users.noreply.github.com>
2024-06-29 12:29:21 +0530

    Update app.js
    search bar

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
+
 const track = document.getElementById('image__track');
 
 window.onmousedown = e => {
   track.dataset.mouseDownAt = e.clientX;
 
-}
-
-window.onmouseup = () => {
-  track.dataset.mouseDownAt = "0";
-  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
 window.onmousemove = e => {
@@ -16,14 +12,35 @@ window.onmousemove = e => {
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
     maxDelta = window.innerWidth/2;
 
-    const percentage = (mouseDelta/maxDelta) * -100,
-    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    const percentage = (mouseDelta/maxDelta) * -100;
+    let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    if(nextPercentage <-100)
+    nextPercentage = -100;
+  else if(nextPercentage >0)
+    nextPercentage = 0;
 
-    console.log("before min and max : " + nextPercentage);
-    Math.min(nextPercentage,0);
-    Math.max(nextPercentage,100);
-    console.log("after min and max : " + nextPercentage);
-    track.dataset.percentage = nextPercentage;
-    track.style.transform = `translate(${nextPercentage}%,0%)`;
+  track.dataset.percentage = nextPercentage;
+
+
+   track.animate({transform:`translate(${nextPercentage}%, 0%)`},{duration: 1200, fill: "forwards"});
+
+  for(const image of track.getElementsByClassName("image")) {
+  image.animate({objectPosition: `${nextPercentage + 100}%  50%`},{duration: 1200, fill: "forwards"});
+   }
+
+
   }
+
+  window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+  }
+
+ const wth = document.getElementsByClassName("test");
+ wth.addEventListener('submit',function f(event){
+  event.preventDefault();
+  let valueBruh = document.getElementsByClassName("test2").value;
+  console.log(valueBruh);
+ })
+
 
